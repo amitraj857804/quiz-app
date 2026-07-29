f86712d4c88465e5b3aef398212be795c4b3276f
Amit Raj <amitraj857804@gmail.com>
2026-07-30 03:31:07 +0530

    fixing minor bugs in the api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://98.88.26.239:8080/api'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://98.88.26.239:8080/api'
 
 function getToken() {
   return localStorage.getItem('token')
